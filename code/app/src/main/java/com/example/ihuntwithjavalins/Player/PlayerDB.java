9ea307d26e05762ddb82c58451dc13e3950d4924
Amogh Panhale <92696483+amoghmpanhale@users.noreply.github.com>
2023-03-09 20:36:49 -0700

    Deleted code to reupload new one

--- code/app/src/main/java/com/example/ihuntwithjavalins/Player/PlayerDB.java
+++
@@ -1,5 +0,0 @@
-package com.example.ihuntwithjavalins.Player;
-
-// Quick note, this class adds information to the user in the database, it does not create more databases or create subcollections
-public class PlayerDB {
-}
--- code/app/src/main/java/com/example/ihuntwithjavalins/QRCode/QRCodeDB.java
+++
@@ -1,5 +0,0 @@
-package com.example.ihuntwithjavalins.QRCode;
-
-// Quick note, this class adds to a subcollection the Player has in the database
-public class QRCodeDB {
-}
